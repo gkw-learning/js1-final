855f82582dee1a128b29a9ee0e098de609a897dd
howdycrystalm <howdycrystalm@gmail.com>
2018-05-06 17:25:18 -0700

    each submit click adds users input to list

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
-// let userInput = document.getElementById('thingToDo'); //stores user's input
+// let userInput = document.getElementById('thingTodo'); //stores user's input
 // let hitSubmit = document.getElementById('submit'); //stores when user hits submit? or just stores button? dunno
 // const submitBtn = add userInput to <h1>To Do</h1><ul><li>
 //
-// document.forms.toDoForm
+// document.forms.TodoForm
 //now make submit button, or enter key capture userInput;
 
 //then add the submission to an array;
@@ -14,29 +14,46 @@
 //make submit button work
 //capture user's input in a variable
 //add that variable to an array per homework instructions
-//have the item in the array show as a <li> in todo list
+//have the item in the array show as a <li> in Todo list
 //wash and repeat
 //maybe create a li on click
 
-//Now that I have the user todo printing to the h2,
-//figure out how to add the user todo to an array.
-//each time the user adds a todo, add another li
+//Now that I have the user Todo printing to the h2,
+//figure out how to add the user Todo to an array.
+//each time the user adds a Todo, add another li
 //immediately invoked function expression aka IIFE;
 (function() {
-  let userInput = document.getElementById('thingToDo'); //this only returns the element, not what user types
-  let form = document.getElementById('toDoForm');
-  let ul = document.getElementById('todoList');
-  let li = document.createElement('li');
-  let arrayOfToDos = [];
+  let userInput = document.getElementById('thingTodo'); //this only returns the element, not what user types
+  let form = document.getElementById('todoForm');
+  let arrayOfTodos = [];
 
   form.addEventListener('submit', e => {
     e.preventDefault(); //stops form from being sent according to my book.
-    let userInput = thingToDo.value;
-    // let msg = 'Welcome ' + userInput;
-    // document.getElementById('theH2').textContent = msg;
-    li.appendChild(document.createTextNode(userInput));
-    let item = ul.appendChild(li);
-    let push = arrayOfToDos.push(item);
-    console.log(arrayOfToDos);
+    let userInput = thingTodo.value; //returns the value of what user types
+    let liNode = document.createElement('li'); //creating li node. must be local scope.
+    let textNode = document.createTextNode(userInput);
+    liNode.appendChild(textNode);
+    document.getElementById('todoList').appendChild(liNode);
+    let push = arrayOfTodos.push(textNode);
+    console.log(arrayOfTodos);
   });
 })();
+
+// (function() {
+//   let userInput = document.getElementById('thingTodo').value; //this only returns the element, not what user types
+//   let form = document.getElementById('todoForm');
+//   let ul = document.getElementById('todoList');
+//   let node = document.createElement('li');
+//   let textNode = document.createTextNode(userInput);
+//   let arrayOfTodos = [];
+//
+//   form.addEventListener('submit', e => {
+//     e.preventDefault(); //stops form from being sent according to my book.
+//     // let userInput = thingTodo.value;
+//     node.appendChild(textNode);
+//     document.getElementById('todoList').appendChild(node);
+//     //let item = ul.appendChild(li);
+//     //let push = arrayOfTodos.push(item);
+//     console.log(arrayOfTodos);
+//   });
+// })();
