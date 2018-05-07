42cfe6c62fc95d07c7c3f95babb799dc419c73cb
howdycrystalm <howdycrystalm@gmail.com>
2018-05-07 00:54:59 -0700

    using sass

--- js/app.js
+++ js/app.js
@@ -1,18 +1,39 @@
+// // //immediately invoked function expression aka IIFE;
+// function addTodo() {
+//   let userInput = document.getElementById('thingTodo'); //this only returns the element, not what user types
+//   let form = document.getElementById('todoForm');
+//   let arrayOfTodos = [];
+//   form.addEventListener('submit' || 'keydown', e => {
+//     e.preventDefault(); //stops form from being sent according to my book.
+//     let userInput = thingTodo.value; //returns the value of what user types
+//     let liNode = document.createElement('li'); //creating li node. must be local scope.
+//     let textNode = document.createTextNode(userInput);
+//     liNode.appendChild(textNode);
+//     document.getElementById('todoList').appendChild(liNode);
+//     let push = arrayOfTodos.push(textNode);
+//     console.log(arrayOfTodos);
+//   });
+// }
+//
+// addTodo();
+
 //immediately invoked function expression aka IIFE;
-function addTodo() {
+(function() {
   let userInput = document.getElementById('thingTodo'); //this only returns the element, not what user types
   let form = document.getElementById('todoForm');
   let arrayOfTodos = [];
-  form.addEventListener('submit' || 'keydown', e => {
-    e.preventDefault(); //stops form from being sent according to my book.
-    let userInput = thingTodo.value; //returns the value of what user types
-    let liNode = document.createElement('li'); //creating li node. must be local scope.
-    let textNode = document.createTextNode(userInput);
-    liNode.appendChild(textNode);
-    document.getElementById('todoList').appendChild(liNode);
-    let push = arrayOfTodos.push(textNode);
-    console.log(arrayOfTodos);
-  });
-}
-
-addTodo();
+  form.addEventListener(
+    'submit' || 'keydown',
+    e => {
+      e.preventDefault();
+      let userInput = thingTodo.value; //returns the value of what user types
+      let liNode = document.createElement('li'); //creating li node. must be local scope.
+      let textNode = document.createTextNode(userInput);
+      liNode.appendChild(textNode);
+      document.getElementById('todoList').appendChild(liNode);
+      let push = arrayOfTodos.push(textNode);
+      console.log(arrayOfTodos);
+    },
+    false
+  );
+})();
