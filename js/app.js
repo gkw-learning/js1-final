0f6468383991929c4a8c3ce3b01bef704139c194
howdycrystalm <howdycrystalm@gmail.com>
2018-05-15 08:28:56 -0700

    broke down code into smaller pieces. radio button changes submit color

--- js/app.js
+++ js/app.js
@@ -31,33 +31,37 @@ let arrayOfTasks = [];
 let bg = document.getElementById('submit');
 
 function createTask() {
-  form.addEventListener(
-    'submit' || 'keydown',
-    e => {
-      e.preventDefault();
-      let userTask;
-      userTask = thingTodo.value;
-      //creating new function by using the constructor function, AddNewTask
-      let newTask = new AddNewTask(userTask);
-      arrayOfTasks.push(newTask);
-      console.log('check it', arrayOfTasks);
-
-      let liNode = document.createElement('li'); //creating li node. must be local scope.
-      let textNode = document.createTextNode(userTask); //grabs value from userTask key
-      liNode.appendChild(textNode); //adds value from userTask to <li>
-      document.getElementById('todoList').appendChild(liNode); //appends the <li> to the <ul>
-      userTask = thingTodo.value = ''; //assign empty string to clear input form after submit clicked
-    },
-    false
-  );
+  document.getElementById('rank').addEventListener('change', e => {
+    e.preventDefault();
+    document.getElementById('submit').style.color = 'red';
+  });
+  //   form.addEventListener(
+  //     'submit' || 'keydown',
+  //     e => {
+  //       e.preventDefault();
+  //       let userTask;
+  //       userTask = thingTodo.value;
+  //       //creating new function by using the constructor function, AddNewTask
+  //       let newTask = new AddNewTask(userTask);
+  //       arrayOfTasks.push(newTask);
+  //       console.log('check it', arrayOfTasks);
+  //
+  //       let liNode = document.createElement('li'); //creating li node. must be local scope.
+  //       let textNode = document.createTextNode(userTask); //grabs value from userTask key
+  //       liNode.appendChild(textNode); //adds value from userTask to <li>
+  //       document.getElementById('todoList').appendChild(liNode); //appends the <li> to the <ul>
+  //       userTask = thingTodo.value = ''; //assign empty string to clear input form after submit clicked
+  //     },
+  //     false
+  //   );
 }
-// createTask();
-// function isChecked() {
-//   radio.addEventListener(if radio is checked){
-//     then change button from grey to green and make it click able.
-//   };
+createTask();
+// // function isChecked() {
+// //   radio.addEventListener(if radio is checked){
+// //     then change button from grey to green and make it click able.
+// //   };
+// // }
+// //the constructor function
+// function AddNewTask(userTask) {
+//   this.userTask = userTask;
 // }
-//the constructor function
-function AddNewTask(userTask) {
-  this.userTask = userTask;
-}
