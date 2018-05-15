5f8f72f867170e382132cc5d398d0da981690212
howdycrystalm <howdycrystalm@gmail.com>
2018-05-15 09:03:22 -0700

    can select any radio to change submit color

--- js/app.js
+++ js/app.js
@@ -24,36 +24,53 @@
 /* maybe dom elements can take place of default values */
 // create a constructor function that makes a new Obj using
 // the input todoForm
-let radio = document.getElementById('rank');
+
+// let radioOnInput = document.getElementsByTagName('input');
+// console.log(radioOnInput);
+// let radio = document.getElementById('rank');
+// console.log(radio);
+
+// let radioOnInput = document.querySelectorAll('input#rank');
+// console.log(radioOnInput);
+// for(let i = 0; i < radioOnInput.length; i++) {
+//   radioOnInput[i].
+// }
+
 let form = document.getElementById('todoForm');
 let arrayOfTasks = [];
 
 let bg = document.getElementById('submit');
 
 function createTask() {
-  document.getElementById('rank').addEventListener('change', e => {
-    e.preventDefault();
-    document.getElementById('submit').style.color = 'red';
-  });
-  //   form.addEventListener(
-  //     'submit' || 'keydown',
-  //     e => {
-  //       e.preventDefault();
-  //       let userTask;
-  //       userTask = thingTodo.value;
-  //       //creating new function by using the constructor function, AddNewTask
-  //       let newTask = new AddNewTask(userTask);
-  //       arrayOfTasks.push(newTask);
-  //       console.log('check it', arrayOfTasks);
-  //
-  //       let liNode = document.createElement('li'); //creating li node. must be local scope.
-  //       let textNode = document.createTextNode(userTask); //grabs value from userTask key
-  //       liNode.appendChild(textNode); //adds value from userTask to <li>
-  //       document.getElementById('todoList').appendChild(liNode); //appends the <li> to the <ul>
-  //       userTask = thingTodo.value = ''; //assign empty string to clear input form after submit clicked
-  //     },
-  //     false
-  //   );
+  let radioOnInput = document.querySelectorAll('input#rank'); //grabs all inputs with id rank
+  for (let i = 0; i < radioOnInput.length; i++) {
+    //loops through lenght of radioInput, whichever is selected canges submit color
+    radioOnInput[i].addEventListener('change', e => {
+      //listens for change on any radio
+      e.preventDefault();
+      bg.style.color = 'red'; //changes color of submit text
+    });
+  }
+
+  form.addEventListener(
+    'submit' || 'keydown',
+    e => {
+      e.preventDefault();
+      let userTask;
+      userTask = thingTodo.value;
+      //creating new function by using the constructor function, AddNewTask
+      let newTask = new AddNewTask(userTask);
+      arrayOfTasks.push(newTask);
+      console.log('check it', arrayOfTasks);
+
+      let liNode = document.createElement('li'); //creating li node. must be local scope.
+      let textNode = document.createTextNode(userTask); //grabs value from userTask key
+      liNode.appendChild(textNode); //adds value from userTask to <li>
+      document.getElementById('todoList').appendChild(liNode); //appends the <li> to the <ul>
+      userTask = thingTodo.value = ''; //assign empty string to clear input form after submit clicked
+    },
+    false
+  );
 }
 createTask();
 // // function isChecked() {
@@ -61,7 +78,7 @@ createTask();
 // //     then change button from grey to green and make it click able.
 // //   };
 // // }
-// //the constructor function
-// function AddNewTask(userTask) {
-//   this.userTask = userTask;
-// }
+//the constructor function
+function AddNewTask(userTask) {
+  this.userTask = userTask;
+}
