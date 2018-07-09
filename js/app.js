b27f969e9fca8894002d42c24f2dad8544dddeee
howdycrystalm <howdycrystalm@gmail.com>
2018-07-08 21:57:27 -0700

    added icons

--- js/app.js
+++ js/app.js
@@ -36,19 +36,20 @@ function createTask() {
       let liNode = document.createElement('li'); //creating li node. must be local scope.
       let textNode = document.createTextNode(task); //grabs value from task key
       let done = document.createElement('button');
-      done.textContent = 'done';
+      done.innerHTML = '<i class="far fa-check-square"></i>';
       done.setAttribute('id', 'doneBtn');
 
       let trash = document.createElement('button');
-      trash.textContent = 'trash';
+      trash.innerHTML = '<i class="fas fa-trash-alt"></i>';
       trash.setAttribute('id', 'trashBtn');
 
       let todoList = document.getElementById('red');
       let green = document.getElementById('green');
       let orange = document.getElementById('orange');
 
-      liNode.appendChild(done);
       liNode.appendChild(trash);
+      liNode.appendChild(done);
+
 
       if (newTask.rank === '1') {
         liNode.appendChild(textNode); //adds value from task to <li>
@@ -68,7 +69,8 @@ function createTask() {
         e => {
           e.preventDefault();
           console.log('you just clicked on done');
-          liNode.style.color = '#a3a19a';
+          liNode.style.color = '#1d1d1d';
+          liNode.style.textDecoration = 'line-through';
         },
         false
       );
